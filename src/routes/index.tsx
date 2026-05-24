1913748dc880f340b9fd61f38960cb9266b926a2
Chris <49985000+schooltwistchris@users.noreply.github.com>
2026-05-24 12:03:30 -0400

    Fix import typo in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-iimport { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 
 export const Route = createFileRoute("/")({
